a8e9136d1060b7a9c2e0813ddd216b40b55369df
samaila-bashir <developer.chatto@gmail.com>
2024-12-20 15:41:28 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
